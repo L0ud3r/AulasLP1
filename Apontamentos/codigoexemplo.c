790dab26d8986d8b1878ce99b8f10daa95b247c4
L0ud3r <playdagamehd@gmail.com>
2020-10-31 14:52:46 +0000

    test

---
+++ Apontamentos/codigoexemplo.c
@@ -0,0 +1,25 @@
+/*
+autor: xxxxx
+email: xxxxx
+date: xxxxx
+desc: (descriçao sobre o programa)
+-
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello\n");
+}
+
+/**
+ * 
+ * indicar valores e o que são
+ * 
+ * 
+ *
+ * */
+int soma(int x, int y)
+{
+    return x + y;
+}
